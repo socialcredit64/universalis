f775852d7c97117676f7c32b1c2a7c8f71631154
socialcredit64 <124119894+socialcredit64@users.noreply.github.com>
2023-11-14 10:03:35 -0600

    Add files via upload

--- src/Economy.java
+++ src/Economy.java
@@ -1,6 +1,29 @@
+import javax.swing.ImageIcon;
 
 public class Economy {
+	private ImageIcon img;
+	private float gold, materials, military_goods, authority, research;
+	private float Dgold, Dmaterials, Dmilitary_goods, Dauthority, Dresearch;
 	public Economy() {
-		
+		gold=100;
+		materials=500;
+		military_goods=0;
+		authority=10;
+		research=0;
+
+		Dgold=5;
+		Dmaterials=10;
+		Dmilitary_goods=1;
+		Dauthority=10;
+		Dresearch=3;
+
+	}
+
+	public void monthTick(){
+		gold+=Dgold;
+		materials+=Dmaterials;
+		military_goods+=Dmilitary_goods;
+		authority+=Dauthority;
+		research+=Dresearch;
 	}
 }
